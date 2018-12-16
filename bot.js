bf9e2eef172b7616f1e05d5aab067a815030b669
n3emxd <45102621+n3emxd@users.noreply.github.com>
2018-12-16 05:56:30 +0300

    Update and rename brodcast.js to bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+var prefix = "+";
+
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "obc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+client.login(process.env.BOT_TOKEN);
